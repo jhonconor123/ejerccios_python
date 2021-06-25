533d90fd3d73fb647b4f8bc4089d968e424fd61a
Elton Zeballos <eltonelzeballos>
2021-06-25 10:04:07 -0400

    Se agrega el proyecto

---
+++ ejercicio_1.py
@@ -0,0 +1,9 @@
+num_1 = float(input("hola mundo:"))
+print(num_1)
+
+num_2 = float(input("hola dos: "))
+print(num_2)
+
+resultado = num_1 + num_2
+
+print("el resultado es",+ resultado)
---
+++ operadores_logicos.py
@@ -0,0 +1,4 @@
+usuario = float(input("CUANTAS HORAS TRBAJO USTED: "))
+coste = float(input("CUANTO COBRA POR HORA: "))
+paga = usuario * coste 
+print ("Tu paga es",paga)
